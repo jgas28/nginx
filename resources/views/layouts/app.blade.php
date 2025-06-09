9db770aae1153d6a18e1cb9f43f2f722670ed436
JGaspay28 <135186818+JGaspay28@users.noreply.github.com>
2025-06-09 20:29:33 +0800

    remove liquidationlist

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -354,9 +354,7 @@
                         <a href="{{ route('liquidations.approvalList') }}" class="block px-3 py-1 rounded hover:bg-gray-700 text-sm">
                             Approved Liquidation
                         </a>
-                        <a href="{{ route('liquidations.liquidationList') }}" class="block px-3 py-1 rounded hover:bg-gray-700 text-sm">
-                            List
-                        </a>
+                       
                     </div>
                 </div>
             @endif
