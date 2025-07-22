a571f239df6046ef96ed46aabdeba886c2606834
JGaspay28 <135186818+JGaspay28@users.noreply.github.com>
2025-07-22 21:22:59 +0800

    Update CashVoucherController.php

--- app/Http/Controllers/CashVoucherController.php
+++ app/Http/Controllers/CashVoucherController.php
@@ -305,6 +305,7 @@ class CashVoucherController extends Controller
             $formattedCvrNumber = "CVR-{$currentYear}-{$currentMonth}-{$nextCvrNumberFormatted}/{$company_id}";
             $user = Auth::user();
             $employeeCode = $user->id;
+            $remarks = $request->has('remarks') ? json_encode($request->remarks) : null;
 
             // Save the actual cash voucher
             $cashVoucher = new CashVoucher([
@@ -318,7 +319,7 @@ class CashVoucherController extends Controller
                 'voucher_type' => $request->voucher_type,
                 'withholding_tax_id' => $request->voucher_type === 'with_tax' ? $request->withholding_tax : null,
                 'tax_based_amount' => $request->voucher_type === 'with_tax' ? $request->tax_base_amount : null,
-                'remarks' => $request->has('remarks') ? $request->remarks : null,
+                'remarks' => $remarks, 
                 'created_by' => $employeeCode,
                 'dr_id' => $request->dr_id,
                 'sequence' => $sequence,
