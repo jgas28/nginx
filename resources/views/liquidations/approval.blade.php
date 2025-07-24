ad9e5e0982107aa69fc1b6f00f1e6193a2659706
JGaspay28 <135186818+JGaspay28@users.noreply.github.com>
2025-07-24 22:24:01 +0800

    Update approval.blade.php

--- resources/views/liquidations/approval.blade.php
+++ resources/views/liquidations/approval.blade.php
@@ -54,6 +54,7 @@
                     class="w-full px-3 py-2 bg-gray-100 text-gray-600 rounded-lg border border-gray-300 cursor-default text-sm"
                 />
             </div>
+            <input type="hidden" name="cash_charge" value="{{ old('cash_charge', $liquidation->cash_charge ?? 0) }}" />
         </div>
     </div>
 
