fafbcea63c43abd50371c34e611fddf3b1bd115b
JGaspay28 <135186818+JGaspay28@users.noreply.github.com>
2025-07-25 16:01:51 +0800

    Update LiquidationController.php

--- app/Http/Controllers/LiquidationController.php
+++ app/Http/Controllers/LiquidationController.php
@@ -239,7 +239,7 @@ class LiquidationController extends Controller
     {
         // Load liquidation with related data
         $liquidation = Liquidation::with('cashVoucher', 'cvrApproval', 'preparedBy', 'notedBy')->findOrFail($id);
-        $employees = User::whereIn('id', [41,5,15])->get(); // You can adjust this condition as needed
+        $employees = User::whereIn('id', [41,5,15,53])->get(); // You can adjust this condition as needed
         $staffs = User::all();
         $approvers = Approver::all();
         $collectors = User::whereIn('id', [15,35,54])->get();
@@ -1038,7 +1038,7 @@ class LiquidationController extends Controller
         // Redirect with success message
         return redirect()->route('liquidations.approval', $liquidation->id)
         ->with('success', 'Liquidation details updated!');
-    }
+    } 
 
     public function approvedCollection(Request $request, $id)
     {
